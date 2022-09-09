273cd87e5b49c8a52068b25f62f2cc450d0a2a8f
Mark Orlovsky <orlovskym@gmail.com>
2022-09-09 15:56:53 -0700

    fix grid squares size bug

--- javascript.js
+++ javascript.js
@@ -2,14 +2,14 @@ let container = document.querySelector(`.container`);
 
 function generateGrid(size) {
     let numSquares = size * size;
-    let squareSize = container.clientWidth / size;
+    let squareSize = container.clientWidth / size - 2;
     console.log(squareSize);
     console.log(numSquares);
 
     for (i = 0; i < numSquares; i++) {
         let square = document.createElement('div');
-        square.style.Height = squareSize;
-        square.style.Width = squareSize;
+        square.style.height = squareSize+"px";
+        square.style.width = squareSize+"px";
         square.classList.add("tile");
         square.style.backgroundColor = "white";
         container.appendChild(square);
@@ -19,7 +19,7 @@ function generateGrid(size) {
 
 function activateSquares() {
     let squares = document.querySelectorAll(`.tile`);
-    squares[0].addEventListener('hover', () => {
+    squares[0].addEventListener('mouseover', () => {
         if (this.backgroundColor = "white") {
             this.backgroundColor = "black";
         } else if (this.backgroundColor = "black") {
